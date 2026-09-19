2a1ab1cf700de5fca42d3826c8c3f2e1d9a7d2a3
Minoru Imura <gaikokujin18@gmail.com>
2026-09-19 17:16:18 +0900

    サイトマップの登録先を人材バンクのトップURLに直す

--- build.js
+++ build.js
@@ -778,7 +778,7 @@ function main() {
       '<?xml version="1.0" encoding="UTF-8"?>',
       '<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">',
       '  <url>',
-      `    <loc>${base}bank.html</loc>`,
+      `    <loc>${base}</loc>`,
       `    <lastmod>${lastmod}</lastmod>`,
       '    <changefreq>weekly</changefreq>',
       '    <priority>1.0</priority>',
